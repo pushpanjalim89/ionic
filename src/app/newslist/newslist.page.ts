a48eb4e84c114521126e1dee6f2de93592ea2f66
PUSHPA_FUTORIX <pushpanjali@futorix.com>
2018-11-22 16:52:41 +0530

    created new page

---
+++ src/app/newslist/newslist.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newslist',
+  templateUrl: './newslist.page.html',
+  styleUrls: ['./newslist.page.scss'],
+})
+export class NewslistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
